61d2c7c75d10822237ad2c515efb4bd93df9dfe5
cherrybaba <1800012710@pku.edu.cn>
2020-11-04 14:59:37 +0800

    lab4

--- Lab4/OLEDSVM.py
+++ Lab4/OLEDSVM.py
@@ -1,14 +1,24 @@
+import time
+import datetime
+import spidev as SPI
+import SSD1306
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 import matplotlib.pyplot as plt
-# Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
-# The digits dataset
+import numpy as np
+
+RST = 19
+DC = 16
+bus = 0
+device = 0
+disp = SSD1306.SSD1306(rst=RST,dc=DC,spi=SPI.SpiDev(bus,device))
+disp.begin()
+disp.clear()
+disp.display()
+
 digits = datasets.load_digits()
-# The data that we are interested in is made of 8x8 images of digits, let's
-# have a look at the first 4 images, stored in the `images` attribute of the
-# dataset. If we were working from image files, we could load them using
-# matplotlib.pyplot.imread. Note that each image must have the same size. For these
-# images, we know which digit they represent: it is given in the 'target' of
-# the dataset.
 images_and_labels = list(zip(digits.images, digits.target))
 for index, (image, label) in enumerate(images_and_labels[:4]):
   plt.subplot(2, 4, index + 1)
@@ -27,15 +37,15 @@ classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
 # Now predict the value of the digit on the second half:
 expected = digits.target[n_samples // 2:]
 predicted = classifier.predict(data[n_samples // 2:])
-
-print("Classification report for classifier %s:\n%s\n"
-      % (classifier, metrics.classification_report(expected, predicted)))
-print("Confusion matrix:\n%s" % metrics.confusion_matrix(expected, predicted))
-
 images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
-for index, (image, prediction) in enumerate(images_and_predictions[:4]):
-  plt.subplot(2, 4, index + 5)
-  plt.axis('off')
-  plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-  plt.title('Prediction: %i' % prediction)
-plt.show()
+for kk,prediction in images_and_predictions:
+    digit=Image.fromarray((kk*8).astype(np.uint8),mode='L').resize((48,48)).convert('1')
+    img=Image.new('1',(disp.width,disp.height),'black')
+    img.paste(digit,(0,16,digit.size[0],digit.size[1]+16))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.load_default()
+    draw.text((0,0),'Prediction: %i' % prediction,font=font,fill=255)
+    disp.clear()
+    disp.image(img)
+    disp.display()
+    time.sleep(1)
--- Lab4/OLEDTest.py
+++ Lab4/OLEDTest.py
@@ -1,6 +1,6 @@
 import time
 import datetime
-import pillow as PIL
+#import Pillow as PIL
 import spidev as SPI
 import SSD1306
 from PIL import Image
@@ -15,8 +15,8 @@ def daysDiff(day2):
     timestamp_day2 = int(time.mktime(time_array2))
     result = (timestamp_day2 - timestamp_day1) // 60 // 60 // 24
     return result
-
 FinalTest="2021-1-22"
+
 RST = 19
 DC = 16
 bus = 0
@@ -25,16 +25,29 @@ disp = SSD1306.SSD1306(rst=RST,dc=DC,spi=SPI.SpiDev(bus,device))
 disp.begin()
 disp.clear()
 disp.display()
-#显示北大logo
-image = Image.open('logo.png').resize((disp.width,disp.height),Image.ANTIALIAS).convert('1')
-disp.image(image)
-disp.display()
 
-font = ImageFont.load_default()
-image1 = Image.new('RGB',(disp.width,disp.height),'black').convert('1')
-draw = ImageDraw.Draw(image1)
-draw.text((x,top),'Hello World',font=font,fill=255)
-draw.text((x,top-10),time.strftime('%Y-%m-%d %H:%M:%S'),font=font,fill=255)
-draw.text((x,top-20),"距期末考试还有：{} 天".format(daysDiff(FinalTest)),font=font,fill=255)
-disp.image(image1)
-disp.display()
+while True:
+  tardate=datetime.datetime(2028,5,4)
+  nowdate=datetime.datetime.now()
+  delta=tardate-nowdate
+  days=delta.days
+  seconds=delta.seconds
+  hours=(seconds-(seconds%3600))/3600
+  seconds=seconds%3600
+  minutes=(seconds-(seconds%60))/60
+  seconds=seconds%60
+  time.sleep(1)
+
+  pkuLogo= Image.open('/home/pi/pku.png').resize((40,40),Image.ANTIALIAS).convert('1')
+  font = ImageFont.load_default()
+  image = Image.new('RGB',(disp.width,disp.height),'black').convert('1')
+  draw = ImageDraw.Draw(image)
+  draw.text((32,0),'Hello World',font=font,fill=255)
+  draw.text((5,15),time.strftime('%Y-%m-%d %H:%M:%S'),font=font,fill=255)
+  draw.text((50,30),"days:{}".format(days),font=font,fill=255)
+  draw.text((50,40),"minutes:{}".format(minutes),font=font,fill=255)
+  draw.text((50,50),"seconds:{}".format(seconds),font=font,fill=255)
+  image.paste(pkuLogo,(0,26))
+  disp.image(image)
+  disp.display()
+
