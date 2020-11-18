781d71f345f8ba09948df76f2326da405b20cfcc
cherrybaba <1800012710@pku.edu.cn>
2020-11-18 14:02:19 +0800

    lab6

--- Lab6/6.1.2.py
+++ Lab6/6.1.2.py
@@ -1,21 +1,33 @@
 import smbus
 import time
+from threading import Thread
 
 address = 0x48
 A0 = 0x40
 bus = smbus.SMBus(1)
 value = 215
 flag = 1
+
+def setSleepTime(address,A0):
+    bus.write_byte(address,A0)
+    sleepTime = bus.read_byte(address)
+    sleepTime = sleepTime/16*0.01
+    return sleepTime
+mythread = Thread(target=callable,args=())
+mythread.start()
 while True:
+    mythread.join()
+    mythread.setDaemon(True)
     #亮度达到最低，标记flag，开始变亮
-    if value == 155:
+    if value == 0:
         flag = 1
     #亮度达到最高，标记flag，开始变暗
-    elif value == 215:
+    elif value == 255:
         flag = 0
     if flag:
         value += 1
     else:
         value -= 1
     bus.write_byte_data(address,A0,value)
-    time.sleep(0.1)
+    
+    time.sleep(setSleepTime(address,A0))
