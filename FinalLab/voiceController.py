3ec67ab6408d9af152594c0561479fc3178a4a78
cherrybaba <1800012710@pku.edu.cn>
2020-12-30 16:16:47 +0800

    voiceController

--- FinalLab/voiceController.py
+++ FinalLab/voiceController.py
@@ -2,12 +2,18 @@ import snowboydecoder
 import sys
 import signal
 import MMC
+import logging
+import time
 
 # Demo code for listening to five hotwords at the same time
 # Models:magicMirror,goodbye,hello,clock,news
 
+logging.basicConfig()
+logger = logging.getLogger("snowboy")
 voiceState="SLEEP"; # WAKEUP,CLOCK,HELLO,NEWS;control gesture
 interrupted = False
+newsFlag=0
+
 
 def singalHandler(signal, frame):
     global interrupted
@@ -21,21 +27,25 @@ def interruptCallback():
 def wakeup():
     global voiceState
     voiceState="WAKEUP"
-    MMC.SetBrightness(100)
+    MMC.setBrightness(100)
     print(voiceState)
+    
         
 def sleep():
     global voiceState
     voiceState="SLEEP"
-    MMC.SetBrightness(0)
+    MMC.setBrightness(0)
     print(voiceState)
+    
         
 def clock():
     global voiceState
     voiceState="CLOCK"
-    MMC.showAlert("Set clock! Got it!",1000)
+    MMC.showAlert("Set clock! Got it!",2000)
+    time.sleep(2)
     MMC.showModule("clock") # 根据手势调整
     print(voiceState)
+    
 
 def hello():
     global voiceState
@@ -43,11 +53,19 @@ def hello():
     MMC.showAlert("Good afterNoon, BaoBeiEr~")
     print(voiceState)
     
+    
 def news():
-    global voiceState
+    global voiceState,newsFlag
     voiceState="NEWS"
     MMC.showAlert("Read the news! Got it!",1000)
+    if newsFlag==0:
+        MMC.readMore()
+        newsFlag=1
+    else:
+        MMC.readLess()
+        newsFlag=0
     print(voiceState)
+    
     
 # confirm the num of models
 if len(sys.argv) != 6:    
@@ -56,13 +74,17 @@ if len(sys.argv) != 6:
     sys.exit(-1)
 models = sys.argv[1:]
 
-MMC.SetBrightness(0)
+MMC.setBrightness(0)
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, singalHandler)
 
 sensitivity = [0.5]*len(models)
 detector = snowboydecoder.HotwordDetector(models, sensitivity=sensitivity)
-callbacks = [wakeup(),sleep(),clock(),hello(),news()]
+callbacks = [lambda: wakeup(),
+             lambda: sleep(),
+             lambda: clock(),
+             lambda: hello(),
+             lambda: news()]
 print('Listening... Press Ctrl+C to exit')
 
 # main loop
