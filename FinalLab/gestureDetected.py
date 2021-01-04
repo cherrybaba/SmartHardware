7091c9aa8fedb59f546a263b6d70d8fd52690e7f
apple <apple@appledeMacBook.local>
2021-01-04 14:16:47 +0800

    combine voice and gesture

--- FinalLab/gestureDetected.py
+++ FinalLab/gestureDetected.py
@@ -5,9 +5,9 @@ from pygame import mixer
 from aip import AipBodyAnalysis
 from aip import AipSpeech
 
-hand={'One':'数字1','Five':'数字5','Fist':'拳头','Ok':'OK',
+hand={'One':'数字1','Five':'Read More','Fist':'Read Less','Ok':'OK',
       'Prayer':'祈祷','Congratulation':'作揖','Honour':'作别',
-      'Heart_single':'比心心','Thumb_up':'点赞','Thumb_down':'Diss',
+      'Heart_single':'比心心','Thumb_up':'Previous News','Thumb_down':'Next News',
       'ILY':'我爱你','Palm_up':'掌心向上','Heart_1':'双手比心1',
       'Heart_2':'双手比心2','Heart_3':'双手比心3','Two':'数字2',
       'Three':'数字3','Four':'数字4','Six':'数字6','Seven':'数字7',
@@ -18,36 +18,27 @@ hand={'One':'数字1','Five':'数字5','Fist':'拳头','Ok':'OK',
 APP_ID = '23434762'
 API_KEY = 'FV8ynuBjMImFgnbDs4EjllQf'
 SECRET_KEY = 'xTrQA5tvZYNG0VQDx2xGepKMtEGw2oRH'
-""" 语音技术 APPID AK SK """
-SpeechAPP_ID = '*******'
-SpeechAPI_KEY ='*******************'
-SpeechSECRET_KEY = '*******************'
 
 camera = PiCamera()
 client = AipBodyAnalysis(APP_ID, API_KEY, SECRET_KEY)
-#Speechclient = AipSpeech(SpeechAPP_ID, SpeechAPI_KEY, SpeechSECRET_KEY)
 
 '''cam config'''
 camera.resolution = (1280, 720)
-#camera.annotate_text = "xuehu96 !" #图片上加水印
-#camera.annotate_background = Color('blue')
-#camera.annotate_text_size = 20
-#camera.annotate_foreground = Color('white')
-#camera.brightness = 55
+
 """ 读取图片 """
 def get_file_content(filePath):
     with open(filePath, 'rb') as fp:
         return fp.read()
 
-mixer.init()
+#mixer.init()
+lastRes=0
 while True:
     """1.拍照 """
     camera.start_preview()
-    time.sleep(2)
-    mixer.music.stop()
+    time.sleep(1)
+    #mixer.music.stop()
     camera.capture('./image.jpg')
     camera.stop_preview()
-
     image = get_file_content('./image.jpg')
 
     """ 2.调用手势识别 """
@@ -58,16 +49,8 @@ while True:
     except:
         print('Nothing detected!' )
     else:
-        print('Detect' + hand[res])
-
-        """ 3.调用文字转语音"""'''
-        content = hand[res]
-        result = Speechclient.synthesis(content, 'zh', 1, {'spd': 2, 'vol': 6, 'per': 1})
-        #print(result)
-        if not isinstance(result, dict):
-            with open('./res.mp3', 'wb') as f:
-                f.write(result)
-            
-            mixer.music.load('./res.mp3')
-            mixer.music.play()
-#            time.sleep(3)'''
+        if res==lastRes:
+            print('Nothing detected!' )
+        else:
+            lastRes=res
+            print('Detect' + hand[res])
--- FinalLab/voiceController.py
+++ FinalLab/voiceController.py
@@ -4,16 +4,94 @@ import signal
 import MMC
 import logging
 import time
+from picamera import PiCamera,Color
+import time
+import demjson
+from pygame import mixer 
+from aip import AipBodyAnalysis
+from aip import AipSpeech
+from threading import thread
 
-# Demo code for listening to five hotwords at the same time
-# Models:magicMirror,goodbye,hello,clock,news
+# Gestures:Thumb_up,Thumb_down,Fist,Five
+# Code for listening to five hotwords at the same time
+# Voice Models:magicMirror,goodbye,hello,clock,news
 
-logging.basicConfig()
-logger = logging.getLogger("snowboy")
-voiceState="SLEEP"; # WAKEUP,CLOCK,HELLO,NEWS;control gesture
-interrupted = False
-newsFlag=0
+hand={'One':'数字1','Five':'Read More','Fist':'Read Less','Ok':'OK',
+      'Prayer':'祈祷','Congratulation':'作揖','Honour':'作别',
+      'Heart_single':'比心心','Thumb_up':'Previous News or clock disappear','Thumb_down':'Next News or clock appear',
+      'ILY':'我爱你','Palm_up':'掌心向上','Heart_1':'双手比心1',
+      'Heart_2':'双手比心2','Heart_3':'双手比心3','Two':'数字2',
+      'Three':'数字3','Four':'数字4','Six':'数字6','Seven':'数字7',
+      'Eight':'数字8','Nine':'数字9','Rock':'Rock','Insult':'竖中指','Face':'脸'}
 
+""" 人体分析 APPID AK SK """
+APP_ID = '23434762'
+API_KEY = 'FV8ynuBjMImFgnbDs4EjllQf'
+SECRET_KEY = 'xTrQA5tvZYNG0VQDx2xGepKMtEGw2oRH'
+
+""" 读取图片 """
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+def gestureDetect():
+    global voiceState,clockFlag
+    lastRes=0
+    while voiceState=="CLOCK" or voiceState=="NEWS":
+        """1.拍照 """
+        camera.start_preview()
+        time.sleep(1)
+        #mixer.music.stop()
+        camera.capture('./image.jpg')
+        camera.stop_preview()
+        image = get_file_content('./image.jpg')
+
+        """ 2.调用手势识别 """
+        raw = str(client.gesture(image))
+        text = demjson.decode(raw) # decode 返回的json
+        try:
+            res = text['result'][0]['classname']
+        except:
+            print('Nothing detected!' )
+        else:
+            if res==lastRes:
+                print('Nothing detected!' )
+            else:
+                lastRes=res
+                print('Detect' + hand[res])
+                if state=="CLOCK":
+                    if clockFlag== "appear" and hand[res]=="Previous News or clock disappear":
+                        #MMC.showAlert("Hide clock! Got it!",2000)
+                        #time.sleep(2)
+                        MMC.hideModule("clock")
+                        clockFlag="disappear"
+                    else if clockFlag=="disappear" and hand[res]== "Next News or clock appear":
+                        #MMC.showAlert("Show clock! Got it!",2000)
+                        #time.sleep(2)
+                        MMC.showModule("clock")
+                        clockFlag="appear"
+                if state == "NEWS":
+                    if hand[res]=="Previous News or clock disappear":
+                        #MMC.showAlert("Previous news! Got it!",2000)
+                        #sleep(2)
+                        MMC.previousArtical()
+                    if hand[res]=="Next News or clock appear":
+                        #MMC.showAlert("Next news! Got it!",2000)
+                        #sleep(2)
+                        MMC.nextArtical()
+                    if hand[res]=="Read More":
+                        #MMC.showAlert("Read the news! Got it!",1000)
+                        MMC.readMore()
+                    if hand[res]=="Read Less":
+                        MMC.readLess()
+# main loop
+# make sure you have the same numbers of callbacks and models
+def voiceDetect():
+    detector.start(detected_callback=callbacks,
+                   interrupt_check=interruptCallback,
+                   sleep_time=0.03)
+
+    detector.terminate()           
 
 def singalHandler(signal, frame):
     global interrupted
@@ -33,40 +111,50 @@ def wakeup():
         
 def sleep():
     global voiceState
-    voiceState="SLEEP"
-    MMC.setBrightness(0)
+    if voiceState=="WAKEUP":
+        voiceState="SLEEP"
+        MMC.setBrightness(0)
+    else:
+        voiceState="WAKEUP" #退出设置
     print(voiceState)
     
         
 def clock():
     global voiceState
-    voiceState="CLOCK"
     MMC.showAlert("Set clock! Got it!",2000)
     time.sleep(2)
-    MMC.showModule("clock") # 根据手势调整
+    voiceState="CLOCK"
     print(voiceState)
     
 
 def hello():
     global voiceState
     voiceState="HELLO"
-    MMC.showAlert("Good afterNoon, BaoBeiEr~")
+    MMC.showAlert("Good afterNoon, BaoBeiEr~"，2000)
+    time.sleep(2)
     print(voiceState)
     
     
 def news():
-    global voiceState,newsFlag
-    voiceState="NEWS"
+    global voiceState
     MMC.showAlert("Read the news! Got it!",1000)
-    if newsFlag==0:
-        MMC.readMore()
-        newsFlag=1
-    else:
-        MMC.readLess()
-        newsFlag=0
+    voiceState="NEWS"
     print(voiceState)
-    
-    
+
+
+
+#camera设置    
+camera = PiCamera()
+client = AipBodyAnalysis(APP_ID, API_KEY, SECRET_KEY)
+'''cam config'''
+camera.resolution = (1280, 720)
+#声音设置
+#logging.basicConfig()
+#logger = logging.getLogger("snowboy") # 终端打印提示信息
+voiceState="SLEEP"; # WAKEUP,CLOCK,HELLO,NEWS;control gesture
+clockFlag="appear" # appear,disappear
+interrupted = False
+
 # confirm the num of models
 if len(sys.argv) != 6:    
     print("Error: need to specify 5 model names")
@@ -87,10 +175,12 @@ callbacks = [lambda: wakeup(),
              lambda: news()]
 print('Listening... Press Ctrl+C to exit')
 
-# main loop
-# make sure you have the same numbers of callbacks and models
-detector.start(detected_callback=callbacks,
-               interrupt_check=interruptCallback,
-               sleep_time=0.03)
+voiceThread = Thread(target=voiceDetect(), args=())
+gestureThread =Thread(target=gestureDetect(), args=())
 
-detector.terminate()
+voiceThread.start()
+gestureThread.start()
+voiceThread.join()
+gestureThread.join()
+
+        
